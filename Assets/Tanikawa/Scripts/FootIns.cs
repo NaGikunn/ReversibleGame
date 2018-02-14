9037c85019a2c0b772b433a67f502422c803d9ee
Unknown <nkc.shota.tanikawa@gmail.com>
2018-02-14 14:57:04 +0900

    足跡UI途中

---
+++ Assets/Tanikawa/Scripts/FootIns.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FootIns : MonoBehaviour
+{
+    GameObject FootB;
+    GameObject FootW;
+
+	// Use this for initialization
+	void Start ()
+    {
+        FootB = (GameObject)Resources.Load("Prefab/P_Tex_Foot(B)");
+        FootW = (GameObject)Resources.Load("Prefab/P_Tex_Foot(W)");
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            Instantiate(FootB);
+            Debug.Log("kuro");
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            Instantiate(FootW);
+            Debug.Log("shiro");
+        }
+    }
+}
