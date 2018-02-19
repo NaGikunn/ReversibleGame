d960fc3ce4fcb1d8cbbc3f50def4a38c042740f4
Unknown <nkc.shota.tanikawa@gmail.com>
2018-02-19 10:57:25 +0900

    Load着手

---
+++ Assets/Tanikawa/Scripts/Load.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Load : MonoBehaviour
+{
+    int[,] panel=new int[5,5];
+    public Sprite PanelW;
+    public Sprite PanelB;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
