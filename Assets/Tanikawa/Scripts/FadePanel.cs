d5cc9a10df43887f749f9781ba1ee7c34457beaa
Unknown <nkc.shota.tanikawa@gmail.com>
2018-02-19 15:42:07 +0900

    Load1種完成

---
+++ Assets/Tanikawa/Scripts/FadePanel.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FadePanel : MonoBehaviour
+{
+     private float FadeMath = 0.1f;
+
+    private Vector3 Scale = Vector3.zero;
+
+    public GameObject Tile;
+
+    private SceneLoad SL;
+
+    void Start(){
+        Tile = gameObject;
+        SL = this.gameObject.transform.parent.GetComponent<SceneLoad>();
+    }
+
+    public void FadeInStart(){
+        Tile.SetActive(true);
+            StartCoroutine(FadeIn());
+    }
+    public void FadeOutStart(){
+            StartCoroutine(FadeOut());
+    }
+
+    public void Reset(){
+        Tile.transform.localScale = new Vector3(Tile.transform.localScale.x,1,Tile.transform.localScale.z);
+        Tile.SetActive(true);
+    }
+
+    private IEnumerator FadeOut(){
+        while (true){
+            if(transform.localScale.y < 0){
+                Tile.SetActive(false);
+                SL.FadeEndTiles++;
+                break;
+            }
+            Scale.y = FadeMath;
+            Tile.transform.localScale -= Scale;
+            yield return new WaitForSeconds(Time.deltaTime);
+        }
+    }
+
+    private IEnumerator FadeIn(){
+        Tile.SetActive(true);
+        while (true){
+            if(transform.localScale.y > 1){
+                SL.FadeEndTiles++;
+                break;
+            }
+            Scale.y = FadeMath;
+            Tile.transform.localScale += Scale;
+            yield return new WaitForSeconds(Time.deltaTime);
+        }
+    }
+}
