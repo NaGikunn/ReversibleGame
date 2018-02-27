119df005afb56013e72ff3d1e38ea0e2ec073953
Unknown <nkc.shota.tanikawa@gmail.com>
2018-02-27 16:01:27 +0900

    制限時間バグ修正完了

--- Assets/Tanikawa/Scripts/TextManeger.cs
+++ Assets/Tanikawa/Scripts/TextManeger.cs
@@ -4,11 +4,13 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class TextManeger : MonoBehaviour {
-
     
-    public Image ImageRes;    //結果発表Image
-    public Image ImageEnd;    //終了Image
+    public Image ImageStandby;//よーいImage
+    public Image ImageSta;    //始めImage
+    public Image ImageNexSta; //ここからImage
     public Image ImageDanger; //回転注意Image
+    public Image ImageEnd;    //終了Image 
+    public Image ImageRes;    //結果発表Image
 
     public GameObject TextMaster3; //親子関係を結ぶCanvas
 
@@ -29,28 +31,51 @@ public class TextManeger : MonoBehaviour {
         }
     }
 
-    //結果発表Image呼び出し
-    public void imaRes()
+    //よーいImage呼び出し
+    public void imaStandby()
     {
-        Image ImePre1 = (Image)Instantiate(ImageRes);
+        Image ImePre1 = (Image)Instantiate(ImageStandby);
         ImePre1.transform.SetParent(TextMaster3.transform, false);
-        //ImePre1.transform.Translate(-1 * Speed, 0, 0);
+        //ImePre3.transform.Translate(1 * Speed, 0, 0);
     }
 
-    //終了Image呼び出し
-    public void imaEnd()
+    //始めImage呼び出し
+    public void imaSta()
     {
-        Image ImePre2 = (Image)Instantiate(ImageEnd);
+        Image ImePre2 = (Image)Instantiate(ImageSta);
         ImePre2.transform.SetParent(TextMaster3.transform, false);
-        //ImePre2.transform.Translate(0, -1 * Speed, 0);
+        //ImePre3.transform.Translate(1 * Speed, 0, 0);
+    }
+
+    //ここからImage呼び出し
+    public void imaNexSta()
+    {
+        Image ImePre3 = (Image)Instantiate(ImageNexSta);
+        ImePre3.transform.SetParent(TextMaster3.transform, false);
+        //ImePre3.transform.Translate(1 * Speed, 0, 0);
     }
 
     //回転注意Image呼び出し
     public void imaDanger()
     {
-        Image ImePre3 = (Image)Instantiate(ImageDanger);
-        ImePre3.transform.SetParent(TextMaster3.transform, false);
+        Image ImePre4 = (Image)Instantiate(ImageDanger);
+        ImePre4.transform.SetParent(TextMaster3.transform, false);
         //ImePre3.transform.Translate(1 * Speed, 0, 0);
     }
 
+    //終了Image呼び出し
+    public void imaEnd()
+    {
+        Image ImePre5 = (Image)Instantiate(ImageEnd);
+        ImePre5.transform.SetParent(TextMaster3.transform, false);
+        //ImePre2.transform.Translate(0, -1 * Speed, 0);
+    }
+
+    //結果発表Image呼び出し
+    public void imaRes()
+    {
+        Image ImePre6 = (Image)Instantiate(ImageRes);
+        ImePre6.transform.SetParent(TextMaster3.transform, false);
+        //ImePre1.transform.Translate(-1 * Speed, 0, 0);
+    }
 }
--- Assets/Tanikawa/Scripts/TimeControl.cs
+++ Assets/Tanikawa/Scripts/TimeControl.cs
@@ -11,40 +11,56 @@ public class TimeControl : MonoBehaviour
     public Text SecondTimeLabel;  // 本番用TimeText
 
     float FirstTime;　　// 最初の10秒（ポイント取り合い）
-    float SecondTime;　 // 本番の60秒（キューブが回り始める）
+    float SecondTime;  // 本番の60秒（キューブが回り始める）
 
-    public static bool secondtimeFlg;　// 本番のTimeを作動させるためflg
+    bool firstTimeflg;   //最初のTimeを作動させるためのflg
+    bool secondTimeflg;
 
     // Use this for initialization
     void Start()
     {
         LoadManeger.SendMessage("ColOut");　// 1番最初にフェードアウトを呼び出す
 
-        secondtimeFlg = false; // 最初の10秒が終わるまで
+        firstTimeflg = false;
+        secondTimeflg = false;
 
         FirstTime = 11.0f;  // 残り時間(表示時にintでキャストするため、初期値をFirstTime+1に)
         SecondTime = 61.0f; // 残り時間(表示時にintでキャストするため、初期値をSecondTime+1に)
 
-        // 本番用Timeはじめは非表示
+        // Timeは最初は非表示
+        FirstTimeLabel.GetComponent<CanvasRenderer>().SetAlpha(0.0f);
         SecondTimeLabel.GetComponent<CanvasRenderer>().SetAlpha(0.0f);
     }
     // Update is called once per frame
     void Update()
     {
-        Invoke("First", 15.0f); //最初用Time遅延
+        Invoke("First", 15.0f);
 
-        // 本番用Time開始
-        if(FirstTime==0.0f)
+       if(firstTimeflg==true)
         {
-            secondtimeFlg = true;
-            Invoke("Second", 5.0f);
+            FirstTimeLabel.GetComponent<CanvasRenderer>().SetAlpha(1.0f);//最初用Timeを表示
+
+            FirstTime -= Time.deltaTime;             // 1フレームにかかる時間を引く
+            FirstTime = Mathf.Max(FirstTime, 0.0f);   // マイナス時間にならないように
         }
 
-        // 本番用Time終了時
-        if (SecondTime == 0.0f)
-        {           
-            LoadManeger.SendMessage("ColIn");
-            return;
+       if (FirstTime==0.0f)
+        {
+            FirstExit();
+        }
+
+       if(secondTimeflg==true)
+        {
+            SecondTimeLabel.GetComponent<CanvasRenderer>().SetAlpha(1.0f);// 本番用Timeを表示
+
+            SecondTime -= Time.deltaTime;             // 1フレームにかかる時間を引く
+            SecondTime = Mathf.Max(SecondTime, 0.0f);   // マイナス時間にならないように
+                                                        // 本番用Time終了時
+            if (SecondTime == 0.0f)
+            {
+                LoadManeger.SendMessage("ColIn");
+                return;
+            }
         }
 
         // 残り時間を2桁で表示
@@ -54,17 +70,22 @@ public class TimeControl : MonoBehaviour
     // 最初用Time
     public void First()
     {
-        FirstTime -= Time.deltaTime;             // 1フレームにかかる時間を引く
-        FirstTime = Mathf.Max(FirstTime, 0.0f);   // マイナス時間にならないように
+        firstTimeflg = true;
+    }
+
+    public void FirstExit()
+    {
+        FirstTimeLabel.GetComponent<CanvasRenderer>().SetAlpha(0.0f);//最初用Timeを非表示
+        Invoke("Second", 5.0f);
+        Debug.Log("Second");
     }
 
     // 本番用Time
     public void Second()
     {
-        FirstTimeLabel.GetComponent<CanvasRenderer>().SetAlpha(0.0f);//最初用Timeを非表示
-        SecondTimeLabel.GetComponent<CanvasRenderer>().SetAlpha(1.0f);// 本番用Timeを表示
-
-        SecondTime -= Time.deltaTime;             // 1フレームにかかる時間を引く
-        SecondTime = Mathf.Max(SecondTime, 0.0f);   // マイナス時間にならないように
+        secondTimeflg = true;
     }
 }
+
+   
+
