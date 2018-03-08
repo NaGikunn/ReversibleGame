9c7e878b7f264357d7785d5971be47d5361a393e
Unknown <nkc.shota.tanikawa@gmail.com>
2018-03-08 15:34:51 +0900

    リザルト演出着手

--- Assets/Tanikawa/Scripts/ResultManeger.cs
+++ Assets/Tanikawa/Scripts/ResultManeger.cs
@@ -1,10 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ResultManeger : MonoBehaviour
 {
     public GameObject LoadManeger2;
+
+    public Image[] ImaResult = new Image[3];
 
 	// Use this for initialization
 	void Start ()
