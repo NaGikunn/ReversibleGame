6438146cc3832f4fbe906f3f96331f2505fe2085
Unknown <nkc.shota.tanikawa@gmail.com>
2018-03-10 00:55:05 +0900

    WIP2

--- Assets/Tanikawa/Scripts/ResultManeger.cs
+++ Assets/Tanikawa/Scripts/ResultManeger.cs
@@ -9,15 +9,30 @@ public class ResultManeger : MonoBehaviour
 
     public Image[] ImaResult = new Image[3];
 
+    public Text[] ImaText = new Text[2];
+
+    GameObject[] ImaSplash = new GameObject[2];
+
 	// Use this for initialization
 	void Start ()
     {
+        ImaSplash[0] = (GameObject)Resources.Load("Prefab/BlackSplash");
+        ImaSplash[1] = (GameObject)Resources.Load("Prefab/WhiteSplash");
+
         LoadManeger2.GetComponent<SceneLoad>().ColOut();
     }
 
 	// Update is called once per frame
 	void Update ()
     {
-        
+        if(Input.GetKeyDown(KeyCode.A))
+        {
+            Instantiate(ImaSplash[0]);
+        }
+
+        if(Input.GetKeyDown(KeyCode.D))
+        {
+            Instantiate(ImaSplash[1]);
+        }
 	}
 }
